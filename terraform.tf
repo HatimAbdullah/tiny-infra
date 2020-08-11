8cfba75bdca2f8ce1695442357d0444ea73db4df
HatimAbdullah <HatimAlqiamh@gmail.com>
2020-08-11 13:34:57 +0300

    added remote state

--- terraform.tf
+++ terraform.tf
@@ -2,5 +2,8 @@ terraform {
   backend "s3" {
     region  = "me-south-1"
     encrypt = true
+    bucket         = "the-sun-must-die"
+    key            = "moon/tiny-infra/terraform.tfstate"
+    dynamodb_table = "the-moon-has-never-been-there"
   }
 }
